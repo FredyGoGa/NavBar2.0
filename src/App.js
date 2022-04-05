2a1af9fce269c0de8a32da254ea8dd30bdd2595f
Fredy Gonzalez Garcia <fredylingling.fgg6@gmail.com>
2022-04-04 22:22:10 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './index.css'
+import Navbar from "./components/Header/Navbar/Navbar";
+
+
+function App() {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header/Navbar/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import "./navbar.css" 
+
+export default function Navbar() {
+  return (
+    <div className="Navbar">
+
+      <div className="Navbar-Item">
+
+       <div className="Item1">Item1</div>
+       <div className="Item2">Item2</div>
+       <div className="Item3">Item3</div>
+       <div className="Item4">Item4</div>
+       
+      </div>  
+
+    </div>
+  )
+}
+
+
+
